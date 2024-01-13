91b7c4623f754c9ea40209deef8ecc5a13189787
Hoan Lac <hoan.lac@outlook.com>
2024-01-13 22:34:00 +0700

    add alacritty config

--- home/vm.nix
+++ home/vm.nix
@@ -41,6 +41,7 @@ in
     users.${username} = { pkgs, lib, ... }: {
       home.stateVersion = "22.11";
       programs.home-manager.enable = true;
+      home.file.".config/alacritty/alacritty.yml".text = builtins.readFile ../files/alacritty.yml;
       home.packages = with pkgs; [
         pkgs._1password-gui
         pkgs.bartender
